608358e78236b4524450118bd7b837a5860dc8d0
Tilen Marš <its0black0magic@gmail.com>
2023-04-19 10:10:36 +0200

    Update main.rb
    Naredi izrisi_mrezo metodo

--- main.rb
+++ main.rb
@@ -1,14 +1,37 @@
 class Mreza
-    def initialize(x,y)
-        @x = 0
-        y = 0
+    def initialize(x, y)
+      @x = 0
+      @y = 0
+      @mreza_array = Array.new(3) { Array.new(3) { 0 } }
     end
-end
 
-class Krog > Mreza
+    def izrisi_mrezo
+      @mreza_array.each do |vrstica|
+        vrstica.each do |stevilo|
+          if stevilo == 0
+            print " "
+          elsif stevilo == -1
+            print "O"
+          elsif stevilo == 1
+            print "X"
+          else
+            print "NAPAKA!!!"
+          end
+        end
+        puts "\n"
+      end
+    end
 
-end
+    def vpisi_v_mrezo(self)
+    end
 
-class Kriz > Mreza
+  end
 
-end
+  class Krog < Mreza
+  end
+
+  class Kriz < Mreza
+  end
+
+  igra_ena = Mreza.new(0, 0)
+  igra_ena.izrisi_mrezo()
